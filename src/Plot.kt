d8851fcb4cb3f82d2bca0d06dc04da19bb605a3a
Gabriel de Souza Seibel <gabrielseibel1@gmail.com>
2017-11-21 17:05:38 -0200

    Changing Plot to be OO

--- src/Plot.kt
+++ src/Plot.kt
@@ -1,19 +1,28 @@
 import org.math.plot.*
 import javax.swing.JFrame
 
+class Plot(x: DoubleArray, y: DoubleArray) {
+
+    private val plot = Plot2DPanel()
+
+    init {
+        // add a line plot to the PlotPanel
+        plot.addLinePlot("my plot", x, y)
+    }
+
+    fun show() {
+        // put the PlotPanel in a JFrame, as a JPanel
+        val frame = JFrame("a plot panel")
+        frame.contentPane = plot
+        frame.isVisible = true
+    }
+}
+
 fun main(args: Array<String>) {
 
     val x = doubleArrayOf(1.0, 2.0, 3.0)
-    val y = doubleArrayOf(1.0, 2.0, 3.0)
+    val y = doubleArrayOf(1.0, 4.0, 9.0)
 
-    // create your PlotPanel (you can use it as a JPanel)
-    val plot = Plot2DPanel()
-
-    // add a line plot to the PlotPanel
-    plot.addLinePlot("my plot", x, y)
-
-    // put the PlotPanel in a JFrame, as a JPanel
-    val frame = JFrame("a plot panel")
-    frame.contentPane = plot
-    frame.isVisible = true
+    val plot = Plot(x, y)
+    plot.show()
 }
